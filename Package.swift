1795a6cc26abd09129dcca95620181fa79ec4dfb
Sander <svantulden@beterdichtbij.nl>
2023-10-16 10:45:48 +0200

    Testing if a wrapper one works

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@
 import PackageDescription
 
 let packageName = "AzureCommunicationCalling"
+let packageWrapperName = packageName + "-Wrapper"
 let package = Package(
     name: packageName,
     platforms: [
@@ -10,18 +11,27 @@ let package = Package(
     products: [
         .library(
             name: packageName,
-            targets: [packageName]
+            targets: [packageWrapperName]
         )
     ],
     dependencies: [
         .package(url: "https://github.com/Azure/SwiftPM-AzureCommunicationCommon", exact: Version("1.1.1"))
     ],
     targets: [
+        .target(
+            name: packageWrapperName,
+            dependencies: [
+                .product(name: "AzureCommunicationCommon", package: "SwiftPM-AzureCommunicationCommon"),
+                .target(name: packageName)
+            ],
+            path: "Sources/WrapperDummy",
+            publicHeadersPath: ""
+        ),
         .binaryTarget(
             name: packageName,
             url: "https://github.com/Azure/Communication/releases/download/v2.5.1/AzureCommunicationCalling-2.5.1.zip",
             checksum: "78d7edb244ce4a31ba6fbb51e596d588747ce97a36ebe89cf953668c0b379758"
         )
     ],
-    swiftLanguageVersions: [.version("5.7.2")]
+    swiftLanguageVersions: [.v5]
 )
