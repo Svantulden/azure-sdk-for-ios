ce8269b70d2d953cf5a3726834cda7e6f5d7817e
Sander <svantulden@beterdichtbij.nl>
2023-10-16 09:26:50 +0200

    Checking specific Swift version

--- Package.swift
+++ Package.swift
@@ -19,9 +19,9 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            url: "https://github.com/Azure/Communication/releases/download/v2.6.1/AzureCommunicationCalling-2.6.1.zip",
-            checksum: "a5af303a56f6349bc8f0c56e03d18ddcae3d607d6eb9ad7a42b0b90bdb631803"
+            url: "https://github.com/Azure/Communication/releases/download/v2.5.1/AzureCommunicationCalling-2.5.1.zip",
+            checksum: "78d7edb244ce4a31ba6fbb51e596d588747ce97a36ebe89cf953668c0b379758"
         )
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageVersions: [.version("5.7.2")]
 )
